4f743161f88aa839635e23919d5cdde795304a2f
jrleolopez <jrleolopez@outlook.com>
2026-03-15 20:03:32 +0000

    configuración segura con .env

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('Error de conexión:', err));
+
+
 // Conexión a la base de datos
 mongoose
-  .connect(process.env.MONGO_URI_)
+.connect(process.env.MONGO_URI_)
   .then(() => console.log("Conexión exitosa a MongoDB"))
   .catch((error) => console.error("Error conectando a MongoDB:", error));
 
