e06260d2123a3f3650bb726df628b7381080d9b2
jrleolopez <jrleolopez@outlook.com>
2026-03-16 16:08:19 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // main.js
 document.addEventListener("DOMContentLoaded", () => {
   const logoutBtn = document.getElementById("logout");
@@ -11,18 +10,4 @@ document.addEventListener("DOMContentLoaded", () => {
     // Redirige al login
     window.location.href = "login.html";
   });
-=======
-// main.js
-document.addEventListener("DOMContentLoaded", () => {
-  const logoutBtn = document.getElementById("logout");
-
-  logoutBtn.addEventListener("click", () => {
-    // Elimina datos de sesión (ejemplo con localStorage)
-    localStorage.removeItem("userToken"); 
-    localStorage.removeItem("categories"); // si guardas categorías
-
-    // Redirige al login
-    window.location.href = "login.html";
-  });
->>>>>>> 1f7a12a9cb29d4bff1a89482d0757af07c3cb3b3
 });
